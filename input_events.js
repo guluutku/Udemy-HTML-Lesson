df210f620ab21c736d68b9b041b60e5d7ef957b9
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-03-01 16:05:54 +0300

    Update input_events.js

---
+++ input_events.js
@@ -0,0 +1,31 @@
+const filter = document.getElementById("filter");
+
+/*
+document.addEventListener("DOMContentLoaded", load);
+function load(e){
+    console.log("Yüklendi");
+}
+*/
+
+// Focus
+filter.addEventListener("focus", run);
+
+// Blur
+filter.addEventListener("blur", run);
+
+// Paste
+filter.addEventListener("paste", run);
+
+// Copy
+filter.addEventListener("copy", run);
+
+// Cut
+filter.addEventListener("cut", run);
+
+// Select
+filter.addEventListener("select", run);
+
+
+function run(e) {
+  console.log(e.type);
+}
