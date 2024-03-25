74691e65c5dd8a16b474bbb5196ce5563d98d59d
Gün ULUUTKU <gunuluutku@gmail.com>
2024-03-25 18:10:23 +0300

    Learned Arrow Functions

---
+++ Arrow Function/app.js
@@ -0,0 +1,10 @@
+/*
+const merhaba = firstName => {
+    console.log("Hello " + firstName);
+}
+*/
+const merhaba = firstName =>     console.log("Hello " + firstName);
+merhaba("Gün");
+
+const cube = x => x * x * x;
+console.log(cube(3));
