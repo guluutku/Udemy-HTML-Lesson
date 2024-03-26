9e702d45882f8cc69bac7183fac0d15ddf8ddb1f
Gün ULUUTKU <gunuluutku@gmail.com>
2024-03-26 20:05:54 +0300

    Learned ES6

---
+++ Statik Methodlar/es6.js
@@ -0,0 +1,14 @@
+// Statik Metodlar:
+class Matematik {
+  static cube(x) {
+    console.log(x * x * x);
+  }
+}
+/* // Dynamic
+const math = new Matematik();
+math.cube(3);
+
+console.log(math);
+*/
+
+Matematik.cube(3);
