8905d9c1caa2ef1e88877747f07b1394c31b2be0
Gün ULUUTKU <gunuluutku@gmail.com>
2024-03-26 21:40:31 +0300

    Add UI dynamicaly

---
+++ project.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini Başlat
+const ui = new UI();
+
+// Tüm Eventleri Yükle
+eventListeners();
+
+function eventListeners() {
+  form.addEventListener("submit", addFilm);
+}
+
+function addFilm(e) {
+  const title = titleElement.value;
+  const director = directorElement.value;
+  const url = urlElement.value;
+
+  if (title === "" || director === "" || url === "") {
+    // Hata
+  } else {
+    const newFilm = new Film(title, director, url);
+
+    ui.addFilmToUI(newFilm);
+  }
+
+  ui.clearInputs(titleElement, directorElement, urlElement);
+  e.preventDefault();
+}
