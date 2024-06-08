0edb6109f7034a8f1dc41f86c7bdc58122f3faef
Gün ULUUTKU <gunuluutku@gmail.com>
2024-06-08 21:53:14 +0300

    Use packages locally and globally

---
+++ Global-Local Packages/paket1/index.js
@@ -0,0 +1,12 @@
+console.log("Paket1");
+
+// Downloaded simple-is locally
+/*
+const is = require("simple-is");
+
+console.log(is.number(24));
+*/
+
+const is = require("simple-is");
+
+console.log(is.number(24));
