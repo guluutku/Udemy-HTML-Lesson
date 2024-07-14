d6058f0ce0bcef35096538ac17fdd07d26817c30
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-07-14 11:47:09 +0300

    Create environment config

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+// Environment Variables
+dotenv.config({
+    path: "./config/env/config.env",
+});
 
 const app = express();
 
-const PORT = 3054;
+const PORT = process.env.PORT;
 
 app.get("/", (req, res)=> {
     res.send("Hello QA API - Updated");
 });
 app.listen(PORT, () => {
-    console.log("App Started on: " + PORT);
+    console.log("App Started on: " + PORT + ` Ortam: ${process.env.NODE_ENV}`);
 });
