5f80ef3888f1186b2e1d576d501f9b3490904784
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-07-16 22:33:16 +0300

    Send data / create collection

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,23 @@
-const register = (req, res, next) => {
+const User = require("../models/user");
+
+const register = async (req, res, next) => {
+
+    // POST DATA
+    const name = "Ali Aliz";
+    const email = "aali@gmail.com";
+    const password = "123456";
+
+    // async await
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
+
     res.status(200).json({
-        success: true
+        success: true,
+        data: user
     });
 };
 
