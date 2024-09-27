9795411ead0e28fac81b93b98a4017307f9ae708
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-09-27 13:18:44 +0300

    Reactstrap giriş

--- intro/src/App.js
+++ intro/src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import Navi from "./Navi";
 import Category from "./CategoryList";
 import Product from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
   return (
     <div >
-      <Navi></Navi>
-      <Category></Category>
-      <Product></Product>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+
+        <Row>
+          <Col xs="3">
+            <Category></Category>
+          </Col>
+
+          <Col xs="9">
+            <Product></Product>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { ListGroupItem, ListGroup } from 'reactstrap'
 
 export default class Category extends Component {
   render() {
     return (
       <div>
         <h3>Category Component</h3>
+        <ListGroup>
+          <ListGroupItem>Item 1</ListGroupItem>
+          <ListGroupItem>Item 2</ListGroupItem>
+          <ListGroupItem>Item 3</ListGroupItem>
+          <ListGroupItem>Item 4</ListGroupItem>
+          <ListGroupItem>Item 5</ListGroupItem>
+        </ListGroup>
       </div>
     )
   }
