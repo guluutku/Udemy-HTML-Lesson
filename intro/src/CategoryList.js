11ed45405ca2bf8973f124b0e4b242d664b165e2
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-09-27 13:59:38 +0300

    No need for constructor

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react'
 import { ListGroupItem, ListGroup } from 'reactstrap'
 
 export default class Category extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiment" }
-      ]
-    };
-  }
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiment" }
+    ]
+  };
 
   render() {
     return (
