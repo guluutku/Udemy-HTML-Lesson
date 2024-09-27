c13d7f140c2838054071669afbed206936a21529
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-09-27 13:43:11 +0300

    Encapsulation

--- intro/src/App.js
+++ intro/src/App.js
@@ -5,20 +5,29 @@ import Product from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
 
 function App() {
+  /*
+  let titleProduct = "Product List";
+  let titleCategory = "Category List";
+  */
+
+  // Encapsulation
+  let categoryInfo = { title: "Category List" };
+  let productInfo = { title: "Product List" };
+  
   return (
     <div >
       <Container>
         <Row>
-          <Navi></Navi>
+          <Navi />
         </Row>
 
         <Row>
           <Col xs="3">
-            <Category></Category>
+            <Category info={categoryInfo} />
           </Col>
 
           <Col xs="9">
-            <Product></Product>
+            <Product info={productInfo} />
           </Col>
         </Row>
       </Container>
--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import { ListGroupItem, ListGroup } from 'reactstrap'
 
 export default class Category extends Component {
+  /* New versions do not need constructors for "<h3>{this.props.title}</h3>"
+  constructor(props) {
+    super(props);
+    state: { }
+  }
+  */
+
   render() {
     return (
       <div>
-        <h3>Category Component</h3>
+        <h3>{this.props.info.title}</h3>
         <ListGroup>
           <ListGroupItem>Item 1</ListGroupItem>
           <ListGroupItem>Item 2</ListGroupItem>
