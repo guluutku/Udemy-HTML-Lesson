9a2dcc6be282053e37806e1918e5a3e070ab940f
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-10-27 15:13:44 +0300

    Error Handling

--- northwind-redux/src/redux/actions/productActions.js
+++ northwind-redux/src/redux/actions/productActions.js
@@ -4,13 +4,63 @@ export function getProductsSuccess(products) {
     return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products }
 }
 
+export function createProductSuccess(product) {
+    return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: product }
+
+}
+
+export function updateProductSuccess(product) {
+    return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: product }
+
+}
+
+// API Function
+export function saveProductApi(product) {
+    return fetch(
+        "http://localhost:3000/products/" + (product.id || ""),
+        {
+            method: product.id ? "PUT" : "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(product)
+        }
+    )
+        .then(handleResponse)
+        .catch(handleError);
+}
+
+// Handle Functions
+export async function handleResponse(response) {
+    if (response.ok) {
+        return response.json();
+    }
+
+    const error = await response.text();
+    throw new Error(error);
+}
+
+// Error Handling
+export function handleError(error) {
+    console.error("Bir Hata Oluştu:  " + error);
+    throw error;
+}
+
+export function saveProduct(product) {
+    return function (dispatch) {
+        return saveProductApi(product).then(savedProduct => {
+            product.id
+                ? dispatch(updateProductSuccess(savedProduct))
+                : dispatch(createProductSuccess(savedProduct));
+        }).catch(e => { throw e });
+    };
+}
+
 export function getProducts(categoryId) {
     return function (dispatch) {
         let url = "http://localhost:3000/products";
         if (categoryId) {
             url = url + "?categoryId=" + categoryId;
         }
-        
+
         return fetch(url)
             .then(response => response.json())
             .then(response => dispatch(getProductsSuccess(response)));
--- northwind-redux/src/redux/reducers/initialState.js
+++ northwind-redux/src/redux/reducers/initialState.js
@@ -8,6 +8,7 @@ export default {
 
     // Product
     products: [],
+    savedProduct:{},
 
     // Cart
     cart: []
