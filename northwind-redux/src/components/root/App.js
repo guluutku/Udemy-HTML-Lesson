9eb3573d72a032306c0666aa703f4eda59d57a40
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-10-27 14:23:50 +0300

    Routing added

--- northwind-redux/src/components/root/App.js
+++ northwind-redux/src/components/root/App.js
@@ -1,14 +1,19 @@
 import { Container } from "reactstrap";
+import { Route, Routes } from "react-router-dom";
 
 import Navi from "../navi/Navi";
 import Dashboard from "./Dashboard";
+import CartDetail from "../cart/CartDetail";
 
 function App() {
   return (
     <Container>
       <Navi />
-      
-      <Dashboard />
+      <Routes>
+        <Route path="/" exact element={<Dashboard />} />
+        <Route path="/product" exact element={<Dashboard />} />
+        <Route path="/cart" exact element={<CartDetail />} />
+      </Routes>
     </Container>
   );
 }
